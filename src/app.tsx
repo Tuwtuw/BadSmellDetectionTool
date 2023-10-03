229fe353eef0297c6da702ef1f37e9867b25a7ef
Eduardo Alves <eduardoalvesvrb@hotmail.com>
2023-10-03 20:03:43 -0300

    Adding db files to git ignore and moving db access to main

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,8 @@
 import * as ReactDOM from 'react-dom';
 import { routes } from './routes';
 import { createHashRouter, RouterProvider } from 'react-router-dom';
-import { useEffect } from 'react';
-import { firstTimeDatabaseInitialization } from './logic/database/database';
 
 const router = createHashRouter(routes);
-
-useEffect(() => {
-  firstTimeDatabaseInitialization();
-}, []);
 
 function render() {
   ReactDOM.render(<RouterProvider router={router} />, document.body);
